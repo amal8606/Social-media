ba281dcb02dc897576f7c10acad4cb51605c0dcc
Amal Abraham <amal.abraham@wenmax.com>
2023-04-28 08:10:03 +0530

    updation with post time

--- src/app/-modules/home/home.component.ts
+++ src/app/-modules/home/home.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { userServices } from 'src/app/-core/services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  constructor(){}
+  constructor(private readonly userService:userServices){}
   public searchFriend:boolean=false;
+  public data:any[]=this.userService.users
+  getTime(postCreatedAt: string): string {
+    const now = new Date();
+    const createdDate = new Date(postCreatedAt);
+    const age = Math.round((now.getTime() - createdDate.getTime()) / (1000 * 60));
+    const newTime=Math.round(age/60)
+    if(age<60){
+      return `${age} minutes ago`;
+
+    }
+    else if(newTime<24){
+      
+       return `${newTime} hours ago`
+    }
+    else if(newTime<42){
+      return `1 day ago`
+    }
+    else{
+      return `${Math.round(newTime/24)} days ago`
+    }
+  }
+  
 }
